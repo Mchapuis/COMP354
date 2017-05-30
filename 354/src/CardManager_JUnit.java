0660da90c0ab0fd6a57e0bc15e6173e300cab4aa
Martine Chapuis <machapu@gmail.com>
2017-05-30 14:33:30 -0400

    update my branch
    update

--- 354/src/CardManager_JUnit.java
+++ 354/src/CardManager_JUnit.java
@@ -1,5 +1,7 @@
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 import org.junit.Test;
 
 public class CardManager_JUnit {
@@ -12,31 +14,39 @@ public class CardManager_JUnit {
 		
 		// getFirstPokemon() 
 		PokemonCard poke_output = cm_test.getFirstPokemon();
-		
-		//assertSame((PokemonCard)card_test, (PokemonCard)poke_output);
+		assertNotSame(card_test, poke_output);
 		
 		// setActivePokemon() 
 		cm_test.setActivePokemon((PokemonCard)poke_output);
-		assertSame(cm_test.activePokemon, (PokemonCard)poke_output);
-		
-		// removeActivePokemon()
-		// discardActivePokemon()
-
-		//---> TODO: loop through discard pile to find a card. 
-		//			the function doesn't exist yet.
+		assertSame(cm_test.getActivePokemon() , (PokemonCard)poke_output);
 		
 		// getFirstEnergy()
-		EnergyCard en_test = new EnergyCard("testing");
-		EnergyCard en_output = cm_test.getFirstEnergy();
-		 
-		//if(en_output != null)
-		//	en_output = new EnergyCard("test");
-		//	assertSame(en_test, en_output);
+		Card en_output = cm_test.getFirstEnergy();
+		boolean isEnergy = false;
+		if(en_output instanceof EnergyCard) isEnergy = true;
+		assertTrue(isEnergy);
+		assertNotNull(en_output);
 		
 		// attachEnergy()
-		// TODO: test if the ID of card is the same
-		//cm_test.attachEnergy(en_output,(PokemonCard)poke_output);
+		cm_test.attachEnergy((EnergyCard)en_output,(PokemonCard)poke_output);
+		assertNotNull(cm_test.getFirstEnergy());
 		
+		// check if the energyCard is in the hand or not
+		ArrayList<Card> hand_test =  cm_test.getHand();
+		for(int i=0; i< hand_test.size(); i++)
+		{
+			// if still in hand...fail the test
+			if(hand_test.equals((EnergyCard)en_output))
+			{
+				fail("Card removed from hand must be deleted");
+			}
+		}
+		
+		// TODO: find cards in the deck.
+		// TODO: find cards in the discard.
+		// TODO: loop through bench
+		// TODO: loop through hand
+		// TODO: loop through prize
 		
 		
 	}
