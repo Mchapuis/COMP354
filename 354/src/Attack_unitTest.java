b5b44f0d9458618f8de1cfdd4fd58dfed7894423
mariegobran <mariegobran@gmail.com>
2017-06-01 14:29:34 -0700

    Updating my branch

--- 354/src/Attack_unitTest.java
+++ 354/src/Attack_unitTest.java
@@ -5,18 +5,14 @@ import org.junit.Test;
 public class Attack_unitTest {
 
 	@Test
-	public void test() {
+	public void Attack_test() {
 		// apply a status 
 		 
 	    Attack attack = new Attack();
 	 
 	    attack.setApplyStatus("paralyzed"); //cannot enter the status in capital form. 
-	 
-	  
-	 
+	
 	    String statusApply = attack.getStatusToApply();
-	 
-	    
 	 
 	    assertEquals("PARALYZED", statusApply);
 	 
