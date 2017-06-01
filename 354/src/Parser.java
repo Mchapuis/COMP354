e4bee1ff990b00c03efd3326e0a75edd31be1ac0
centipedes <kylaslea@gmail.com>
2017-06-01 01:30:20 -0400

    v1.3 of Parser.java
    -> readInCards() method complete
    -> created parseAttacks(), a static void method which generates attack objects
    -> created addAttacks(), a static void method which adds created attack objects to their respective card object
    
    please do not be shy and comment out the various sysprints, they just tell you what's going on in the code

--- 354/src/Parser.java
+++ 354/src/Parser.java
@@ -2,14 +2,6 @@ import java.io.*;
 import java.util.*;
 
 public class Parser {
-
-	/* 
-	 * TODO: Figure out how to change parsed string segments into enums
-	 * 			(alternatively find out if the enums will be useful at all or whether they're excessive/unnecessary)
-	 * 		 Finish parsing the card info and fully instantiating card methods aka categories/attacks/abilities/etc of each card type
-	 * 		 Eventually figure out how to relate all of this information into GUI versions of the cards
-	 * 		 Create deck() method
-	 *
 	
 /* v 1.0
  * what does the parser do? 
@@ -17,9 +9,9 @@ public class Parser {
    				--> currently the input card text file [cards.txt]
 		- Currently arranges the various Card Objects listed in [cards.txt], 
 				--> aka the total deck, via various ArrayLists 
-	Abstract Card class contains super methods for the various Card types
+		- Abstract Card class contains super methods for the various Card types
 				--> Energy; Pokemon; Trainer cards, and eventually their sub-categories
-	For now am using equals() method to determine card type
+		- For now am using a String equals() method to determine card type
 	
 	x - x - x - x - x - x - x - x - x - x - x - x - x - x - x - x - x - x - x - x - x - x
 	
@@ -38,30 +30,41 @@ public class Parser {
 	x - x - x - x - x - x - x - x - x - x - x - x - x - x - x - x - x - x - x - x - x - x
 	 
 	 changes v1.3:
-	 	- readinabilities and parseabilitylines;
-	 	- going to integrate nfrank's ability parsing
+	 	- modulated readincards so that part of it exists in parsecardlines
+	 	- created parseattacks method, which creates attack objects and adds them to an array list of attack objects called totalAttackList
+	 	- created method called addattacks which adds attacks to the cards' instantiated blank attack array list
+	 	- TODO: integrate nfrank's ability parsing
+	 		    figure out how to relate all of this information into GUI versions of the cards
+	 		    change so that an uninstantiated card is created instead of manually incrementing card count id in readInCards when faced with a blank line '#' (this is good praxis)
 		
 */
 	
 	// Reads in cards.txt into an array list of strings. 
 		// (mostly just copied and tweaked ana's code)
+	
 	public static void readInCards(){
+		
 		String fileName = "cards.txt";
 		String line = null;
 		ArrayList<String> cardLines = new ArrayList<String>();
 		
 		try{
+			
 			FileReader fileReader = new FileReader(fileName);
 			BufferedReader bufferedReader = new BufferedReader(fileReader);
 			
-			while (!(line = bufferedReader.readLine()).isEmpty()){ // changed because it wasn't catching null lines
+			while (!(line = bufferedReader.readLine()).isEmpty()){ 
 				cardLines.add(line);
 			}
 			
 			bufferedReader.close();
+			
 		} catch (FileNotFoundException e){
+			
 			System.out.println("File " + fileName + " not found.");
+			
 		} catch (IOException e){
+			
 			e.printStackTrace();
 		}
 		
@@ -71,49 +74,141 @@ public class Parser {
 	public static void parseCardLine(ArrayList<String> cardLines){	
 		
 		// array which contains card objects
+		
 				ArrayList<Card> newCards = new ArrayList<Card>(); 
-
+				int count = 0;
+				
 				// creates rudimentary cards of the three card types out of only some of the information located in cards.txt
 				// if '#' sysprints blank and increments card number allocator
+				
 				for (String l : cardLines){
-					String[] cS = l.split("[,:\\n]");
+					
+					String[] cS = l.split(":");
+					
 					try {
+						
 						if (cS[0].equals("#")){
+							
 							Card.cardnumber++;
 							System.out.println("Blank");
 						}
+						
 						else if (cS[1].equals("pokemon")) {
+							
 							// depending on the stage of the pokemon card, uses two different constructors, one of which references basic stage evolution via name
+						
 							if (cS[3].equals("basic")){
-								newCards.add(new PokemonCard(cS[0], cS[3], cS[5], Integer.parseInt(cS[6])));
+								
+								newCards.add(new PokemonCard(cS[0], cS[3], cS[5], Integer.parseInt(cS[6]), Integer.parseInt(cS[10])));
 								System.out.println("New Basic Pokemon card created.");
-							}
-							else if (cS[3].equals("stage-one")){
-								newCards.add(new PokemonCard(cS[0], cS[3], cS[4], cS[6], Integer.parseInt(cS[7])));
+							}	
+							else if (cS[3].equals("stage-one")){	
+									
+								newCards.add(new PokemonCard(cS[0], cS[3], cS[4], cS[6], Integer.parseInt(cS[7]), Integer.parseInt(cS[11])));
 								System.out.println("New Stage-One Pokemon card created.");
 							}
 						}
+						
 						else if (cS[1].equals("trainer")){
+							
 							newCards.add(new TrainerCard(cS[0], cS[3], Integer.parseInt(cS[4])));
 							System.out.println("New Trainer card created.");
+							
 						}
+						
 						else if (cS[1].equals("energy")) {
+							
 							newCards.add(new EnergyCard(cS[0], cS[3])); 
 							System.out.println("New Energy card created.");
 						}
+						
 					} catch (ArrayIndexOutOfBoundsException o){
+						
 							System.out.println("There was a problem parsing and creating cards");
 							o.printStackTrace();
 						}
 				}
 				
 				// prints out tostrings of the cards that were created
+				
 				for (Card c : newCards) {
+					
 					System.out.println(c.toString());
 				}
+				
+				parseAttacks(newCards, cardLines);
 	}
 	
-	// only changed one thing; buffered reader now catches null lines
+	// parses Attacks from cards.txt, creates Attack objects which are related to cardID (parsed as attackID and should be equal), and adds them to a totalAttackList array list
+	
+	public static void parseAttacks(ArrayList<Card> newCards, ArrayList<String> cardLines)	{
+		
+		ArrayList<Attack> totalAttackList = new ArrayList<Attack>();
+		int attackID = 1;
+
+		for (String l : cardLines)	{
+			
+			String[] cS = l.split(":");
+			
+			if (l.equals("#"))	{
+				attackID++;
+				continue;
+				}
+			
+			else if (cS[1].equals("pokemon"))	{
+				
+				String[] attacksString = l.split("attacks:");
+				String[] multipleAttacksString = attacksString[1].split(",");
+				String[] multipleAttacksArray = new String[4];
+				
+				for (String s : multipleAttacksString){
+					
+					String maa3 = "0";
+					multipleAttacksArray = s.split(":");
+					
+					if (multipleAttacksArray.length == 4)	{ 
+						
+						maa3 = multipleAttacksArray[3];
+						}
+					
+					totalAttackList.add(new Attack(attackID, multipleAttacksArray[1], Integer.parseInt(multipleAttacksArray[2]), Integer.parseInt(maa3)));
+					System.out.println("Attack successfully created. Attack is related to CarID number "+attackID);
+				}
+			}
+			attackID++;
+		}
+		
+		for (Attack a : totalAttackList) {
+			
+			System.out.println(a.toString());
+		}
+		
+		addAttacks(newCards, totalAttackList);
+	}
+	
+	public static void addAttacks (ArrayList<Card> newCards, ArrayList<Attack> totalAttackList)	{
+		
+			for (Card c : newCards)	{	
+				
+				int compareID = c.cardID();
+				
+				for(Attack a : totalAttackList) {
+						
+					if (c.getType() == "pokemon" && a.getAttackID() == compareID) {		
+						
+						((PokemonCard) c).addAttack(a);
+						}
+					}
+				}
+			
+			for (Card c : newCards){
+				
+				System.out.println(c.toString());
+				System.out.println(((PokemonCard) c).getAttacks());
+			}
+		}
+			
+
 		public static void readInAbilities(){
 			String fileName = "abilities.txt";
 			String line = null;
@@ -123,7 +218,7 @@ public class Parser {
 				FileReader fileReader = new FileReader(fileName);
 				BufferedReader bufferedReader = new BufferedReader(fileReader);
 				
-				while (!(line = bufferedReader.readLine()).isEmpty()){ // changed because it wasn't catching null lines
+				while (!(line = bufferedReader.readLine()).isEmpty()){ 
 					abilityLines.add(line);
 				}
 				
@@ -148,6 +243,7 @@ public class Parser {
 			
 		}
 	}
+
 
 	// -------------------------------------------MAIN--------------------------------------------------------
 	
