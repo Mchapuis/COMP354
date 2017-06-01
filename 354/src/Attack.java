df38b9a7e12f37e8c26e4744ad8ad0d42c7a28eb
centipedes <kylaslea@gmail.com>
2017-06-01 01:38:12 -0400

    This version of Attack works with v1.3 of Parser
    Again, this Attack class may be dissimilar to what Anastasia has written and intends the Attack class to do. 
    I can definitely change the name of this class, these just create the Attack objects that are parsed from Cards.txt, and are added into the Attack Array List that is instantiated into every PokemonCard object

--- 354/src/Attack.java
+++ 354/src/Attack.java
@@ -1,7 +1,63 @@
-public class Attack {
 
-	private String name;
-	private int damagePoints;
-	private int[] energyRequired;
+public class Attack extends PokemonCard{
 	
+	private int attackID;
+	private int energyTokens;	// number of energy cards it takes
+	private int abilityID;		// reference id 
+	
+	//private EnergyCard energyType;
+	private String energyType; //?? should it be an energy card OR a string delineating type
+	
+	public Attack()	{
+		this.attackID = attackID;	// ???
+		this.energyTokens = energyTokens;
+		this.energyType = energyType;
+		this.abilityID = abilityID;
+	}
+	
+	public Attack(int attackID, String energyType, int energyTokens, int abilityID)	{
+		setAttackID(attackID);
+		setEnergyTokens(energyTokens);
+		setEnergyType(energyType);
+		setAbilityID(abilityID);
+	}
+	
+	public void setEnergyTokens(int energyTokens)	{
+		this.energyTokens = energyTokens;
+	}
+	
+	public void setEnergyType(String energyType)	{
+		this.energyType = energyType;
+	}
+	
+	public void setAbilityID(int abilityID)	{
+		this.abilityID = abilityID;
+	}
+	
+	public void setAttackID(int attackID)	{
+		this.attackID = attackID;
+	}
+	
+	public int getAttackID()	{
+		return attackID;
+	}
+	
+	public int getEnergyTokens(){
+		return energyTokens;
+	}
+	
+	public String getEnergyType()	{
+		return energyType;
+	}
+	
+	public int getAbilityID()	{
+		return abilityID;
+	}
+	
+	public String toString()	{
+		
+		return ("[AttackID: " +getAttackID()+" | energy type: "+getEnergyType()+" | energy number: "+getEnergyTokens()+" | abilityID: "+getAbilityID()+"]");
+		
+	}
+
 }
