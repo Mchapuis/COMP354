873f3a237e030c37cb8d6e2ebd6d9ac10f629891
mariegobran <mariegobran@gmail.com>
2017-07-14 16:00:29 -0700

    few changes on old unit tests and integration

--- 354/src/PokemonCard_unitTest.java
+++ 354/src/PokemonCard_unitTest.java
@@ -8,26 +8,38 @@ public class PokemonCard_unitTest {
 	public void pokemonHasEnergy_test() {
 		DeenergizeAbility ability = new DeenergizeAbility();
 		PokemonCard pokemon1 = new PokemonCard();
-		EnergyCard energy = new EnergyCard("COLORLESS");
+		EnergyCard energy = new EnergyCard("fight");
+		System.out.println(energy.getType().toString());
 	
 		ability.addEnergyRequired(energy.getType(), 1); 
 		pokemon1.addAbility(ability);
 	
 		boolean hasEnergy = pokemon1.hasEnoughEnergyForAttack(0);
 		
-		assertFalse(hasEnergy);
+		assertFalse(hasEnergy); //check that the pokemon doesn't have enough energy
 		
 		pokemon1.attachEnergy(energy);
 		hasEnergy = pokemon1.hasEnoughEnergyForAttack(0);
 		
-		assertTrue(hasEnergy);
+		assertTrue(hasEnergy); // check if the energy was added
+		
+		// testing pokemon status change
+		
+		PokemonCard pokemon2 = new PokemonCard();
+		Status currentStatus = pokemon2.getStatus();
+		
+		assertSame(currentStatus, Status.NORMAL);
+		
+		// changing Status
+		pokemon2.applyStatus(Status.PARALYZED);
+		currentStatus = pokemon2.getStatus();
+		assertSame(currentStatus, Status.PARALYZED);
+		
+		
 	}
 	
-//	public void pokemonStatusChange(){
-//		PokemonCard pokemon2 = new PokemonCard();
-//		pokemon2.applyStatus("PARALYZED");
-//		
-//		
-//	}
+	
+		
+		
 
 }
